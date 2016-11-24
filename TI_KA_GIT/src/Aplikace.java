83a147d8633f7917173cf5e03c1acc06bf3fcca1
Martin Matas <nouzovekonto@gmail.com>
2016-11-24 17:59:55 +0100

    funkce 31-37

--- TI_KA_GIT/src/Aplikace.java
+++ TI_KA_GIT/src/Aplikace.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Datum vytvoření: 23.11.2016.
  *
@@ -6,7 +8,37 @@
 public class Aplikace {
 
     public static void main(String[] args) {
-        
+        int p_vody = 1;
+        int teplota = 0;
+        int poloha_kelimku = 0;
+        int nadrz_plna = 0;
+        int p_kelimku = 10;
+        int p_cukru = 10;
+        int p_smes1 = 20;
+        int c_k1 = 20;
+        int p_smes2 = 10;
+        int c_k2 = 15;
+        int p_smes3 = 20;
+        int c_k3 = 20;
+        int p_jedna = 100;
+        int p_dva = 60;
+        int p_peti = 50;
+        int p_deseti = 30;
+        int p_dvaceti = 20;
+        int p_padesati = 20;
+
+        Automat a = new Automat(p_vody,teplota,poloha_kelimku,nadrz_plna,p_kelimku,p_cukru,p_smes1,c_k1,p_smes2,c_k2,p_smes3,c_k3,p_jedna,p_dva,p_peti,p_deseti,p_dvaceti,p_padesati);
+
+        a.vhozeniMince(50);
+        a.vhozeniMince(20);
+        a.vhozeniMince(50);
+        a.vhozeniMince(5);
+        System.out.println("Počty mincí v mincovníku: " + Arrays.toString(a.p_minci));
+        System.out.println("Kolik peněz je: " + a.penize);
+        a.init_pen_vrat();
+        System.out.println("Kolik peněz zbylo: " + a.penize);
+        System.out.println("Počty mincí k vrácení: " + Arrays.toString(a.pen_vrat));
+
     }
     
     //commit
--- TI_KA_GIT/src/Automat.java
+++ TI_KA_GIT/src/Automat.java
@@ -4,33 +4,207 @@
  * @author Martin Matas
  */
 public class Automat {
-    int max_cukru = 5, min_cukru = 0, cukr = 3;
-    int p_kelimku = 20;
-    int p_vody = 0; //cidlo_vody[0,1]
+    final int MIN_CUKRU = 0;
+    final int CUKR_START_VAL = 3;
+    final int MAX_POVOLENO_CUKRU = 5;
+    final int MAX_CUKRU;
+
+    final int NAPOJ_1 = 1;
+    final int NAPOJ_2 = 2;
+    final int NAPOJ_3 = 3;
+
+    // inicializuji se pri vytvoreni podle cidla
+    int druh_k;
+    int cukr; // defaultni hodnota cukru
+    int p_vody = 1; //cidlo_vody[0,1]
     int teplota = 0;    //cidlo_teploty[?]
     int poloha_kelimku = 0; // cidlo[0,1]
     int nadrz_plna = 0;  // cidlo_hladiny[0,1]
-    String vypis = "";
 
-    int p_smes1 = 20;
-    int c_k1 = 20;
+    // resetuji se
+    String vypis;
+    int penize; // stav vhozenych penez
+    int[] pen_vrat; //[p1,p2,p5,p10,p20,p50] pole pro vraceni
 
-    int p_smes2 = 15;
-    int c_k2 = 25;
-
-    int p_smes3 = 20;
-    int c_k3 = 15;
-
-    int penize = 0; // stav vhozenych penez
-    int[] druh_k = {1,2,3};
-
+    // zasoby automatu a cena smesi
+    int p_kelimku;
+    int p_cukru;
+    int p_smes1;
+    int c_k1;
+    int p_smes2;
+    int c_k2;
+    int p_smes3;
+    int c_k3;
     // pocty korun v mincovniku
-    int p_jedna = 100;
-    int p_dva = 80;
-    int p_peti = 50;
-    int p_deseti = 40;
-    int p_dvaceti = 30;
-    int p_padesati = 20;
-    int[] pen_vrat = {0,0,0,0,0,0}; //[p1,p2,p5,p10,p20,p50] pole pro vraceni
-    
+    int[] p_minci;
+    /*int p_jedna;
+    int p_dva;
+    int p_peti;
+    int p_deseti;
+    int p_dvaceti;
+    int p_padesati;*/
+
+    public Automat(int p_vody, int teplota, int poloha_kelimku, int nadrz_plna, int p_kelimku, int p_cukru, int p_smes1, int c_k1, int p_smes2, int c_k2, int p_smes3, int c_k3, int p_jedna, int p_dva, int p_peti, int p_deseti, int p_dvaceti, int p_padesati) {
+        this.MAX_CUKRU = getMaxCukru(p_cukru);
+        this.cukr = getCukrStartVal(p_cukru);
+
+        this.p_vody = p_vody;
+        this.teplota = teplota;
+        this.poloha_kelimku = poloha_kelimku;
+        this.nadrz_plna = nadrz_plna;
+        this.p_kelimku = p_kelimku;
+        this.p_cukru = p_cukru;
+
+        this.p_smes1 = p_smes1;
+        this.c_k1 = c_k1;
+        this.p_smes2 = p_smes2;
+        this.c_k2 = c_k2;
+        this.p_smes3 = p_smes3;
+        this.c_k3 = c_k3;
+
+        this.p_minci = new int[6];
+        this.p_minci[0] = p_jedna;
+        this.p_minci[1] = p_dva;
+        this.p_minci[2] = p_peti;
+        this.p_minci[3] = p_deseti;
+        this.p_minci[4] = p_dvaceti;
+        this.p_minci[5] = p_padesati;
+
+        this.vypis = "";
+        this.penize = 0; // stav vhozenych penez
+        this.pen_vrat = new int[6];//[p1,p2,p5,p10,p20,p50] pole pro vraceni
+    }
+
+    /**
+     * Při vytvoření instance se nastaví maximální možná hodnota cukru na MAX_POVOLENO_CUKRU, pokud není tolik
+     * cukru na skladě, nastaví se na hodnotu cukrSkladem
+     *
+     * @param cukrSkladem - množství cukru (kostek) skladem
+     * @return - maximální hranice počtu kostek cukru (max 5) podle zásob cukru
+     */
+    private int getMaxCukru(int cukrSkladem) {
+        int maxCukru = MAX_POVOLENO_CUKRU;
+        if(cukrSkladem < MAX_POVOLENO_CUKRU) maxCukru = cukrSkladem;
+
+        return maxCukru;
+    }
+
+    /**
+     * Při vytvoření se nastaví defaultní hodnota kostek cukru na 3, pokud není tolik kostek skadem, upraví se
+     * hodnota na hodnotu cukrSkladem
+     *
+     * @param cukrSkladem - množství cukru (kostek} skladem
+     * @return - defaultní hodnota cukru, která se nastaví pro automat a zobrazí na display
+     */
+    private int getCukrStartVal(int cukrSkladem) {
+        int cukrDefault = CUKR_START_VAL;
+        if(cukrSkladem < CUKR_START_VAL) cukrDefault = cukrSkladem;
+
+        return cukrDefault;
+    }
+
+    /**
+     * Metoda zkontroluje jestli je na skladě alespoň jedna směs ze všech nabízených
+     *
+     * @return - směs skladem = true, směs není skladem = false
+     */
+    public boolean zkontrolujSmesi() {
+        if (this.p_smes1 > 0 || this.p_smes2 > 0 || this.p_smes3 > 0) {
+            return true;
+        } else {
+            System.out.println("Směsi nejsou na skladě");
+            return false;
+        }
+    }
+
+    /**
+     * Metoda zvětší počet kostek cukru, které se přidají do nápoje o jednu, maximálně však na hodnotu MAX_CUKRU
+     */
+    public void plusC() {
+        if((this.cukr + 1) <= MAX_CUKRU) {
+            this.cukr++;
+        }
+    }
+
+    /**
+     * Metoda zmenší počet kostek, které se přidají do nápoje o jednu, minimálně však na hodnotu MIN_CUKRU
+     */
+    public void minusC() {
+        if((this.cukr - 1) >= MIN_CUKRU) {
+            this.cukr--;
+        }
+    }
+
+    /**
+     * Metoda přičte vhozenou minci k celkové sumě, kterou uživatel do automatu už naházel
+     *
+     * @param mince - mince 1,2,5,10,20 nebo 50
+     * @return - je to mince = true, není to mince = false
+     */
+    public boolean vhozeniMince(int mince) {
+        boolean jeMince = false;
+
+        switch(mince) {
+            case 1:
+                penize += mince;
+                p_minci[0]++;
+                jeMince = true;
+                break;
+            case 2:
+                penize += mince;
+                p_minci[1]++;
+                jeMince = true;
+                break;
+            case 5:
+                penize += mince;
+                p_minci[2]++;
+                jeMince = true;
+                break;
+            case 10:
+                penize += mince;
+                p_minci[3]++;
+                jeMince = true;
+                break;
+            case 20:
+                penize += mince;
+                p_minci[4]++;
+                jeMince = true;
+                break;
+            case 50:
+                penize += mince;
+                p_minci[5]++;
+                jeMince = true;
+                break;
+            default:
+                System.out.println("Není mince");
+                break;
+        }
+
+        return jeMince;
+    }
+
+    public boolean zkontolujVstupy() {
+        return (
+                (druh_k == NAPOJ_1 && penize >= this.c_k1 && this.p_smes1 > 0) ||
+                (druh_k == NAPOJ_2  && penize >= this.c_k2 && this.p_smes2 > 0) ||
+                (druh_k == NAPOJ_3 && penize >= this.c_k3 && this.p_smes3 > 0)
+        );
+    }
+
+    public boolean init_pen_vrat() {
+        boolean presne = false;
+        int[] druhy_minci = {1,2,5,10,20,50};
+        int pocet;
+
+        for (int i = (druhy_minci.length - 1); i >= 0 ; i--) {
+            pocet = penize/druhy_minci[i];
+            if(pocet > p_minci[i]) continue;
+            pen_vrat[i] = pocet;
+            penize %= druhy_minci[i];
+        }
+
+        if(penize == 0) presne = true;
+
+        return presne;
+    }
 }
